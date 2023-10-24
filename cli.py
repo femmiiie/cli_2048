1a585992b91376ea84ed448413e59537e429275c
femmiiie <sandro.mocev@gmail.com>
2023-10-24 10:15:10 -0400

    added cli intro text

--- cli.py
+++ cli.py
@@ -1,5 +1,17 @@
+from time import sleep
+
 def print_board(board:list)->None:
     for i in board:
         for j in i:
             print(j, end=" ")
         print()
+
+def intro()->None:
+    print("Welcome to the CLI version of 2048!")
+    print("Objective: combine like values to reach 2048 without running out of space")
+    print("Controls:")
+    print(" w: up")
+    print(" a: left")
+    print(" s: down")
+    print(" d: right")
+    sleep(3)
