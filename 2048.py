2b95b2a976de635089d7dc9a2ca77771bdae66e2
femmiiie <sandro.mocev@gmail.com>
2023-10-24 10:14:42 -0400

    started board class and finished main function

--- 2048.py
+++ 2048.py
@@ -1,18 +1,48 @@
-#creates 4x4 2048 board
-def board_init()->list:
-    return [
+import sys
+import random
+
+class Board:
+    def __init__(self, seed)->None:
+        #inits empty board
+        self.board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
+        random.seed = seed
+        self.val = 0
+        self.row = 0
+        self.col = 0
+
+    def generate_rand(self)->None:
+        pass
+
+    def insert(self)->None:
+        pass
 
 
-def main(*args):
-    #
-    if args[0] == "cli":
-        import cli as game
-    elif args[0] == "gui":
+def main():
+    #intialize board
+    board = Board(1234)
+    
+    #import based on cli arguments
+    #if no args provided, defaults to gui
+    try:
+        if sys.argv[1] == "cli":
+            import cli as game
+        elif sys.argv[1] == "gui":
+            import gui as game
+        else:
+            print("Usage: 2048.py [cli, gui]")
+            exit()
+    except:
         import gui as game
+
+    #send into respective loops based on import
+    #game.loop()
+
+if __name__ == "__main__":
+    main()
 
     
